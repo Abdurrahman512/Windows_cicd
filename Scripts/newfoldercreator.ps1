88e1ea552f5d3e1f407106c5012ed84223a1d77e
Abdurrahman512 <76415954+Abdurrahman512@users.noreply.github.com>
2022-02-23 13:14:03 +0530

    Update newfoldercreator.ps1

--- Scripts/newfoldercreator.ps1
+++ Scripts/newfoldercreator.ps1
@@ -1,13 +1,5 @@
 #!/powershell
 
 echo "creating new folder in C drive"
-New-Item -Path "C:\t\t\t" -Name "test" -ItemType "directory"
-Set-Content C:\t\t\t\test\file.txt 'Welcome to NEW WORLD'
-Remove-Item 'C:\t\t\t\test\file.txt' -Recurse
-Remove-Item 'C:\t\t\t\test' -Recurse
-Remove-Item 'C:\t\t\t' -Recurse
-Remove-Item 'C:\t\t' -Recurse
-Remove-Item 'C:\t' -Recurse
-New-Item -Path "C:\t\t\t" -Name "test" -ItemType "directory"
-Set-Content C:\t\t\t\test\file.txt 'Welcome to NEW WORLD'
-Remove-Item 'C:\t\t\t\test\file.txt' -Recurse
+New-Item -Path "C:\1" -Name "test1" -ItemType "directory"
+Set-Content C:\1\test1\file.txt 'appspec1'
